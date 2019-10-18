a6dc7e53a8e92ee042d98d2c768b14940902a38f
t0shi <t0shi@MacBook-Pro-t0shi.local>
2019-10-18 23:02:20 +0200

    first commit

---
+++ lab1/stars-catalog/src/main/java/pjatk/tau/s15661/App.java
@@ -0,0 +1,13 @@
+package pjatk.tau.s15661;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
