4ae0f71db65ab9debef7f091f98eb25465f661a6
t0shi <t0shi@MacBook-Pro-t0shi.local>
2019-11-25 00:04:52 +0100

    Rest Configuration Folder

--- lab4/starsCatalog/src/main/java/pl/dymek/tau/starscatalog/service/RedGigantStarService.java
+++ lab4/starsCatalog/src/main/java/pl/dymek/tau/starscatalog/service/RedGigantStarService.java
@@ -1,5 +1,6 @@
 package pl.dymek.tau.starscatalog.service;
 
+import org.springframework.stereotype.Repository;
 import pl.dymek.tau.starscatalog.domain.models.RedGigantStar;
 import pl.dymek.tau.starscatalog.domain.models.Model;
 import pl.dymek.tau.starscatalog.service.exceptions.ObjectAlreadyExistException;
@@ -7,6 +8,7 @@ import pl.dymek.tau.starscatalog.service.exceptions.ObjectDoesNotExistException;
 
 import java.util.List;
 
+@Repository
 public interface RedGigantStarService {
     public void create(RedGigantStar model) throws ObjectAlreadyExistException;
     public List<RedGigantStar> readAll();
